35c846a5c72b99d6727f3c4c2ff8d7890fe5db60
ritikj0536 <jharitik64@gmail.com>
2025-12-16 11:27:15 +0530

    show-hide

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [showPara,setShowPara] = useState(false);
+  const handleClick = () =>{
+    setShowPara(true);
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={showPara ? "show" : "hide"}>
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
+      <button id="click" onClick={handleClick}>
+        onClick{handleClick}
+      </button>
     </div>
   );
 }
